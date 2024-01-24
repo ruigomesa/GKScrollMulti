243e69fd60352022ff03a2ffd98ee05d4ffd4660
Rui Gomes <ruigomes.a@gmail.com>
2024-01-24 23:26:22 +0000

    Update GKScroll.cpp

--- src/GKScroll.cpp
+++ src/GKScroll.cpp
@@ -9,9 +9,12 @@
 
 
 #include "Arduino.h"
-#include "GKScroll.h"
+#include "GKScrollMulti.h"
 
 int lineOneStep, lineTwoStep = 0;
+int line1OneStep, line1TwoStep = 0;
+int line2OneStep, line2TwoStep = 0;
+int line3OneStep, lineT3woStep = 0;
 
 
 
@@ -37,6 +40,82 @@ int  stringLength = lineToScrollLeft.length();
  }
  String str = lineToScrollLeft.substring(lineOneStep,stringEndPosition);
  lineOneStep++;
+ return str;
+   
+}
+
+String ScrollLine1(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
+  
+int  stringLength = lineToScrollLeft.length();
+ if(line1OneStep > lineToScrollLeft.length() + scrollBuffer){
+   line1OneStep = 0;
+ }
+ int stringEndPosition = line1OneStep + lcdColumns;
+ if (stringEndPosition > (lineToScrollLeft.length())){
+  int difference = stringEndPosition - lineToScrollLeft.length();
+  
+   for(int i=0; i < scrollBuffer; i++){
+ lineToScrollLeft = lineToScrollLeft + " ";
+  }
+  for(int i=0; i < (difference - scrollBuffer); i++){
+    int iplus1 = i + 1;
+  String tempString = lineToScrollLeft.substring(i,iplus1);  
+  lineToScrollLeft = lineToScrollLeft + tempString ;  
+  }
+  
+ }
+ String str = lineToScrollLeft.substring(line1OneStep,stringEndPosition);
+ line1OneStep++;
+ return str;
+   
+}
+String ScrollLine2(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
+  
+int  stringLength = lineToScrollLeft.length();
+ if(line2OneStep > lineToScrollLeft.length() + scrollBuffer){
+   line2OneStep = 0;
+ }
+ int stringEndPosition = line2OneStep + lcdColumns;
+ if (stringEndPosition > (lineToScrollLeft.length())){
+  int difference = stringEndPosition - lineToScrollLeft.length();
+  
+   for(int i=0; i < scrollBuffer; i++){
+ lineToScrollLeft = lineToScrollLeft + " ";
+  }
+  for(int i=0; i < (difference - scrollBuffer); i++){
+    int iplus1 = i + 1;
+  String tempString = lineToScrollLeft.substring(i,iplus1);  
+  lineToScrollLeft = lineToScrollLeft + tempString ;  
+  }
+  
+ }
+ String str = lineToScrollLeft.substring(line2OneStep,stringEndPosition);
+ line2OneStep++;
+ return str;
+   
+}
+String ScrollLine3(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
+  
+int  stringLength = lineToScrollLeft.length();
+ if(line3OneStep > lineToScrollLeft.length() + scrollBuffer){
+   line3OneStep = 0;
+ }
+ int stringEndPosition = line3OneStep + lcdColumns;
+ if (stringEndPosition > (lineToScrollLeft.length())){
+  int difference = stringEndPosition - lineToScrollLeft.length();
+  
+   for(int i=0; i < scrollBuffer; i++){
+ lineToScrollLeft = lineToScrollLeft + " ";
+  }
+  for(int i=0; i < (difference - scrollBuffer); i++){
+    int iplus1 = i + 1;
+  String tempString = lineToScrollLeft.substring(i,iplus1);  
+  lineToScrollLeft = lineToScrollLeft + tempString ;  
+  }
+  
+ }
+ String str = lineToScrollLeft.substring(line3OneStep,stringEndPosition);
+ line3OneStep++;
  return str;
    
 }
