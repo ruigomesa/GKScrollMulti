b67145f3a06a71f756cb78d3e7e6195fb2550e6a
Rui Gomes <ruigomes.a@gmail.com>
2024-02-03 01:46:46 +0000

    Update GKScrollMulti.cpp

--- src/GKScrollMulti.cpp
+++ src/GKScrollMulti.cpp
@@ -11,112 +11,31 @@
 #include "Arduino.h"
 #include "GKScrollMulti.h"
 
-int lineOneStep, lineTwoStep = 0;
-int line1OneStep, line1TwoStep = 0;
-int line2OneStep, line2TwoStep = 0;
-int line3OneStep, lineT3woStep = 0;
+int lineStep[] = {0, 0, 0, 0};
 
 
+String ScrollLine(String lineToScrollLeft, int scrollBuffer, int lcdColumns, int lcdLine, int reset) {
+  int  stringLength = lineToScrollLeft.length();
+  if (lineStep[lcdLine] > lineToScrollLeft.length() + scrollBuffer || reset == 1) {
+    lineStep[lcdLine] = 0;
+  }
+  int stringEndPosition = lineStep[lcdLine] + lcdColumns;
+  if (stringEndPosition > (lineToScrollLeft.length())) {
+    int difference = stringEndPosition - lineToScrollLeft.length();
 
-String ScrollLine(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
-  
-int  stringLength = lineToScrollLeft.length();
- if(lineOneStep > lineToScrollLeft.length() + scrollBuffer){
-   lineOneStep = 0;
- }
- int stringEndPosition = lineOneStep + lcdColumns;
- if (stringEndPosition > (lineToScrollLeft.length())){
-  int difference = stringEndPosition - lineToScrollLeft.length();
-  
-   for(int i=0; i < scrollBuffer; i++){
- lineToScrollLeft = lineToScrollLeft + " ";
-  }
-  for(int i=0; i < (difference - scrollBuffer); i++){
-    int iplus1 = i + 1;
-  String tempString = lineToScrollLeft.substring(i,iplus1);  
-  lineToScrollLeft = lineToScrollLeft + tempString ;  
-  }
-  
- }
- String str = lineToScrollLeft.substring(lineOneStep,stringEndPosition);
- lineOneStep++;
- return str;
-   
-}
+    for (int i = 0; i < scrollBuffer; i++) {
+      lineToScrollLeft = lineToScrollLeft + " ";
+    }
+    for (int i = 0; i < (difference - scrollBuffer); i++) {
+      int iplus1 = i + 1;
+      String tempString = lineToScrollLeft.substring(i, iplus1);
+      lineToScrollLeft = lineToScrollLeft + tempString ;
+    }
 
-String ScrollLine1(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
-  
-int  stringLength = lineToScrollLeft.length();
- if(line1OneStep > lineToScrollLeft.length() + scrollBuffer){
-   line1OneStep = 0;
- }
- int stringEndPosition = line1OneStep + lcdColumns;
- if (stringEndPosition > (lineToScrollLeft.length())){
-  int difference = stringEndPosition - lineToScrollLeft.length();
-  
-   for(int i=0; i < scrollBuffer; i++){
- lineToScrollLeft = lineToScrollLeft + " ";
   }
-  for(int i=0; i < (difference - scrollBuffer); i++){
-    int iplus1 = i + 1;
-  String tempString = lineToScrollLeft.substring(i,iplus1);  
-  lineToScrollLeft = lineToScrollLeft + tempString ;  
-  }
-  
- }
- String str = lineToScrollLeft.substring(line1OneStep,stringEndPosition);
- line1OneStep++;
- return str;
-   
-}
-String ScrollLine2(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
-  
-int  stringLength = lineToScrollLeft.length();
- if(line2OneStep > lineToScrollLeft.length() + scrollBuffer){
-   line2OneStep = 0;
- }
- int stringEndPosition = line2OneStep + lcdColumns;
- if (stringEndPosition > (lineToScrollLeft.length())){
-  int difference = stringEndPosition - lineToScrollLeft.length();
-  
-   for(int i=0; i < scrollBuffer; i++){
- lineToScrollLeft = lineToScrollLeft + " ";
-  }
-  for(int i=0; i < (difference - scrollBuffer); i++){
-    int iplus1 = i + 1;
-  String tempString = lineToScrollLeft.substring(i,iplus1);  
-  lineToScrollLeft = lineToScrollLeft + tempString ;  
-  }
-  
- }
- String str = lineToScrollLeft.substring(line2OneStep,stringEndPosition);
- line2OneStep++;
- return str;
-   
-}
-String ScrollLine3(String lineToScrollLeft, int scrollBuffer, int lcdColumns){
-  
-int  stringLength = lineToScrollLeft.length();
- if(line3OneStep > lineToScrollLeft.length() + scrollBuffer){
-   line3OneStep = 0;
- }
- int stringEndPosition = line3OneStep + lcdColumns;
- if (stringEndPosition > (lineToScrollLeft.length())){
-  int difference = stringEndPosition - lineToScrollLeft.length();
-  
-   for(int i=0; i < scrollBuffer; i++){
- lineToScrollLeft = lineToScrollLeft + " ";
-  }
-  for(int i=0; i < (difference - scrollBuffer); i++){
-    int iplus1 = i + 1;
-  String tempString = lineToScrollLeft.substring(i,iplus1);  
-  lineToScrollLeft = lineToScrollLeft + tempString ;  
-  }
-  
- }
- String str = lineToScrollLeft.substring(line3OneStep,stringEndPosition);
- line3OneStep++;
- return str;
-   
+  String str = lineToScrollLeft.substring(lineStep[lcdLine], stringEndPosition);
+  lineStep[lcdLine]++;
+  return str;
+
 }
 
